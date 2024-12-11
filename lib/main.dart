acb4acea42191c6cf6281cbb50e6b6896013be97
ameer-devconst <ameer@devconst.com>
2024-12-11 20:43:39 +0500

    imp firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,8 +12,13 @@ import 'package:kaosity_app/utils/route_generator.dart';
 import 'package:kaosity_app/utils/screen_bindings.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await GetStorage.init();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final box = GetStorage();
